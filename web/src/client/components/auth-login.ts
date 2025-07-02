b62b2c9d2cfb86355985dfebb5cdc22ae454f122
Peter Steinberger <steipete@gmail.com>
2025-07-02 21:26:17 +0100

    fix: remove debug stack trace logging from auth-login component
    - Remove accidentally committed console.log with stack trace from handleOpenSettings
    - Clean up production code by removing debug statements

--- web/src/client/components/auth-login.ts
+++ web/src/client/components/auth-login.ts
@@ -145,8 +145,6 @@ export class AuthLogin extends LitElement {
   }
 
   private handleOpenSettings = () => {
-    console.log('🔧 Auth-login: handleOpenSettings called');
-    console.log('Stack trace:', new Error().stack);
     // Don't bubble - let parent handle via direct event listener
     this.dispatchEvent(new CustomEvent('open-settings'));
   };
