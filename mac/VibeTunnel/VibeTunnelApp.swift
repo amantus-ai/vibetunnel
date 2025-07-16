f304144f3d824cbbbe31bce045e0204459380bd1
Peter Steinberger <steipete@gmail.com>
2025-07-16 02:38:30 +0200

    fix: additional SwiftFormat modifier order issue

--- mac/VibeTunnel/VibeTunnelApp.swift
+++ mac/VibeTunnel/VibeTunnelApp.swift
@@ -124,7 +124,7 @@ struct VibeTunnelApp: App {
 @MainActor
 final class AppDelegate: NSObject, NSApplicationDelegate, @preconcurrency UNUserNotificationCenterDelegate {
     // Needed for some gross menu item highlight hack
-    static weak var shared: AppDelegate?
+    weak static var shared: AppDelegate?
     override init() {
         super.init()
         Self.shared = self
