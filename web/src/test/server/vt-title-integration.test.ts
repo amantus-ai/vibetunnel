959dc4320f53c970448411f952963ab98164a57f
Peter Steinberger <steipete@gmail.com>
2025-07-24 17:55:39 +0200

    fix: remove unused import in vt-title-integration test

--- web/src/test/server/vt-title-integration.test.ts
+++ web/src/test/server/vt-title-integration.test.ts
@@ -1,6 +1,5 @@
 import { exec } from 'child_process';
 import * as fs from 'fs/promises';
-import * as fsSync from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import { promisify } from 'util';
